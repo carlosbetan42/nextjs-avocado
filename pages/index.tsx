023651b986668082e38479e46752283f725d8c4a
Carlos <carlos.betan.42@gmail.com>
2021-10-12 17:11:54 -0500

    Static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,13 @@ import Layout from "@components/Layout/Layout";
 import KawaiiHeader from "@components/KawaiiHeader/KawaiiHeader";
 import ProductList from "@components/ProductList/ProductList";
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   // page only
   const PROTOCOL = process.env.PROTOCOL;
   const URL = process.env.BKND_URL;
 
   // const response = await fetch(`${PROTOCOL}${URL}/api/avo`);
-  const response = await fetch(`https://nextjs-avocado-aqekkeocm-carlosbetan42.vercel.app/api/avo`);
+  const response = await fetch(`https://nextjs-avocado-c95meimeg-carlosbetan42.vercel.app/api/avo`);
   const { data: productList }: TAPIAvoResponse = await response.json();
 
   return {
