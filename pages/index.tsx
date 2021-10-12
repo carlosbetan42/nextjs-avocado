da18b9c628134a088ae83a9e7e2b72d89a0ce90c
Carlos <carlos.betan.42@gmail.com>
2021-10-12 16:57:13 -0500

    Server side

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import Layout from "@components/Layout/Layout";
 import KawaiiHeader from "@components/KawaiiHeader/KawaiiHeader";
 import ProductList from "@components/ProductList/ProductList";
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   // page only
   const PROTOCOL = process.env.PROTOCOL;
   const URL = process.env.BKND_URL;
 
-  const response = await fetch(`${PROTOCOL}${URL}/api/avo`);
+  // const response = await fetch(`${PROTOCOL}${URL}/api/avo`);
+  const response = await fetch(`https://nextjs-avocado-aqekkeocm-carlosbetan42.vercel.app/api/avo`);
   const { data: productList }: TAPIAvoResponse = await response.json();
 
   return {
